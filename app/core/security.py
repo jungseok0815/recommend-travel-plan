dbc5e5811037344aa4ac83b5743a1b89c112c093
DESKTOP-CB5R3AI\USER <wjdtjr9401@naver.com>
2026-04-27 16:25:26 +0900

    update jwt

--- app/core/security.py
+++ app/core/security.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timedelta
 from jose import jwt, JWError
-from app.core.config import  SECRET_KEY, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES
+from app.core.config import  SECRET_KEY, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES,REFRESH_TOKEN_EXPIRE_MINUTES
 
 def create_access_token(data: dict):
     to_encode = data.copy()
@@ -8,9 +8,16 @@ def create_access_token(data: dict):
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
-def decode_access_token(token: str):
+def create_refresh_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=REFRESH_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+def decode_token(token: str):
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithm=[ALGORITHM])
         return payload
     except JWError:
         return None
+
