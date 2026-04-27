c15e8e8a0b5fc8b9baec6169fdd4aa66b44fe45a
DESKTOP-CB5R3AI\USER <wjdtjr9401@naver.com>
2026-04-27 11:25:57 +0900

    add user

---
+++ app/domain/user/models/userModel.py
@@ -0,0 +1,10 @@
+  from sqlalchemy import Column, Integer, String            
+  from app.db.database import Base
+
+  Class User(Base)
+    __tablename__ = "Users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String(100), unique=True, nullable=False)
+    password = Column(String(255), nullable=False)
+    address = Column(String(255), nullable=False)
