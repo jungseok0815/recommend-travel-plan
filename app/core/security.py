b963206ff30752659387ba185aae0303bd13379c
DESKTOP-CB5R3AI\USER <wjdtjr9401@naver.com>
2026-04-27 09:29:09 +0900

    update jwt인증

---
+++ app/core/security.py
@@ -0,0 +1,16 @@
+from datetime import datetime, timedelta
+from jose import jwt, JWError
+from app.core.config import  SECRET_KEY, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+def decode_access_token(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithm=[ALGORITHM])
+        return payload
+    except JWError
+        return None
