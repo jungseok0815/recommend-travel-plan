987f23122796c32b2f4b8970198e9ade8b9667bf
DESKTOP-CB5R3AI\USER <wjdtjr9401@naver.com>
2026-04-29 14:04:52 +0900

    add keword search test

--- test/external/test_tour_api.py
+++ test/external/test_tour_api.py
@@ -1,4 +1,6 @@
-from app.external.tour_api import get_spots_by_area,get_spot_detail
+from app.external.tour_api import get_spots_by_area,get_spot_detail,search_spots_by_keyword
+## test 실행방법 
+## pytest test/external/test_tour_api.py -v -s
 
 def test_get_spots_by_area():
     result = get_spots_by_area("제주")
@@ -7,6 +9,16 @@ def test_get_spots_by_area():
 
 # test_get_spots_by_area()
 
+def test_search_spots_by_keyword():
+    keyword = "카페"
+    area_name="제주"
+
+    result = search_spots_by_keyword(keyword, area_name)
+    print(result)
+    assert len(result) > 0
+
+test_search_spots_by_keyword()
+
 def test_get_spot_detail():
     contentId = 600584
     contentTypeId = 12
@@ -14,6 +26,9 @@ def test_get_spot_detail():
     print(result)
     assert len(result) > 0
 
-test_get_spot_detail()
+# test_get_spot_detail()
+
+
+
 
 
